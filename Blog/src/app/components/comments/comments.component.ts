412cbbd9eaad45bfa5543f4211ce4cd2be8c8682
josemanuelmtz <martinezgarciajosemanuel225@gmail.com>
2024-07-01 20:27:37 -0600

    Back-end

--- Blog/src/app/components/comments/comments.component.ts
+++ Blog/src/app/components/comments/comments.component.ts
@@ -1,4 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+//import { CommentsService } from '../../services/comments.service';
+
+interface Comment {
+  id?: number;
+  postId: number;
+  userId: number;
+  content: string;
+  createdAt?: string;
+  updatedAt?: string;
+}
 
 @Component({
   selector: 'app-comments',
@@ -7,4 +17,11 @@ import { Component } from '@angular/core';
 })
 export class CommentsComponent {
 
+  @Input() postId!: number;
+  comments: Comment[] = [];
+  newComment: string = '';
+
+  constructor() { }
+
+  
 }
